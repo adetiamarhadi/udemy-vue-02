6ec3b795cb1aa42b99b8961d2975b754f713a30f
adetiamarhadi <adetia.marhadi@gmail.com>
2021-11-27 15:34:22 +0700

    Methods used for Data Binding: How It Works

--- app.js
+++ app.js
@@ -6,6 +6,13 @@ const app = Vue.createApp({
     };
   },
   methods: {
+    outputFullname() {
+      console.log('Running again...');
+      if (this.name === '') {
+        return '';
+      }
+      return this.name + ' ' + 'Kurniawan';
+    },
     resetInput() {
       this.name = '';
     },
